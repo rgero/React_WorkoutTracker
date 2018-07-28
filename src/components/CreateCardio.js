56bdb4e28846f5d794ff443c82ac1d9295752a13
Roy Gero <roy.gero.ii@gmail.com>
2018-07-27 22:35:13 -0400

    Adding the basic Cardio stuff.

--- src/components/CreateCardio.js
+++ src/components/CreateCardio.js
@@ -1,15 +1,68 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import {addExercise} from '../actions/exercise';
+
 export class CreateCardio extends React.Component {
     constructor(props){
         super(props);
         this.onSubmit = this.onSubmit.bind(this);
+        this.onTextChange = this.onTextChange.bind(this);
+        this.handleValueChange = this.handleValueChange.bind(this);
+
+        this.state= {
+            name: props.exercise ? props.exercise.name : '',
+            type: props.exercise ? props.exercise.type : 'cardio',
+            distance: props.exercise ? props.exercise.distance : '0',
+            time: props.exercise ? props.exercise.distance : '0',
+            workoutID: props.workoutID ? props.workoutID : '',
+            error: ''
+        };
     }
 
-    onSubmit(){
-        this.props.history.push('/')
+    onSubmit(e){
+        e.preventDefault();
+        var error = '';
+        if(!this.state.name){
+            error = "Please provide a name"
+            this.setState(()=>({error}))
+        } else {
+            this.setState(()=>({error}))
+            const newExercise = {
+                name: this.state.name,
+                type: "cardio",
+                distance: this.state.distance,
+                time: this.state.time
+            }
+            this.props.onSubmit(newExercise);
+            this.props.history.push(`/create/${newExercise.workoutID}/`)
+        }
     }
+
+    onTextChange = (value) => (evt) => {
+        const name = evt.target.value;
+        this.setState({
+            name
+        })
+    }
+
+    handleValueChange = (key) => (evt) => {
+        const amount = evt.target.value;
+        if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+            if (key === "distance"){
+                this.setState({
+                    distance: amount
+                })
+            } else if (key === "time"){
+                this.setState({
+                    time: amount
+                })
+            }
+        } else {
+
+        }
+    }
+
 
     render(){
         return(
@@ -20,9 +73,35 @@ export class CreateCardio extends React.Component {
                     </div>
                 </div>
                 <div className="content-container">
-                    <h1>CARDIO</h1>
-
-
+                    <form className="form">
+                            {this.state.error !== '' && <p className="form__error">{this.state.error}</p>}
+                            <span className="label">Name</span>
+                            <input
+                                type="text"
+                                className="text-input"
+                                placeholder="Name"
+                                autoFocus
+                                value={this.state.name}
+                                onChange={this.onTextChange("name")}
+                            />
+                            <span className="label">Time</span>
+                            <input
+                                type="text"
+                                placeholder="0"
+                                value={this.state.time}
+                                onChange={this.handleValueChange('time')}
+                            />
+                            <span className="label">Distance</span>
+                            <input
+                                type="text"
+                                placeholder="0"
+                                value={this.state.distance}
+                                onChange={this.handleValueChange('distance')}
+                            />
+                    </form>
+                    <div>
+                        <button className="button" onClick={this.onSubmit}>Save Exercise</button>
+                    </div>
                 </div>
             </div>
         )
@@ -31,7 +110,14 @@ export class CreateCardio extends React.Component {
 
 const mapDispatchToProps = (dispatch) => {
     return {
+        onSubmit: (exercise) => dispatch(addExercise(exercise))
     }
 }
 
-export default connect(undefined, undefined)(CreateCardio); // Check out the react-redux documentation to understand the connect statement here.
+const mapStateToProps = (state, props) => {
+    return {
+        workoutID: props.match.params.workoutID ? props.match.params.workoutID : uuid()
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateCardio); // Check out the react-redux documentation to understand the connect statement here.
