36a2506c335601b2906543ac3d75855e13d83f15
Roy Gero <roy.gero.ii@gmail.com>
2023-02-25 11:27:47 -0500

    Removing the offset since I'm not storing the time, I'm just storing the date and that's at midnight local time.

--- workout_tracker/src/helpers/DateFormatter.js
+++ workout_tracker/src/helpers/DateFormatter.js
@@ -1,6 +1,5 @@
 export default (targetDate) => {
     let formattedDate = targetDate;
-    const offset = formattedDate.getTimezoneOffset()
-    formattedDate = new Date(formattedDate.getTime() - (offset*60*1000))
-    return formattedDate.toISOString().split('T')[0]
+    formattedDate = new Date(formattedDate.getTime());
+    return formattedDate.toISOString().split('T')[0];
 }
