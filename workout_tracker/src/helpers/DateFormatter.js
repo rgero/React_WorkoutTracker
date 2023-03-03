704f12361ed40a6be9918fe719b873a4cb1aa886
Roy Gero <roy.gero.ii@gmail.com>
2023-03-03 15:50:22 -0500

    Improving the Date Formatter so that it can take a string. That way I don't have to continually cast it to a date object.

--- workout_tracker/src/helpers/DateFormatter.js
+++ workout_tracker/src/helpers/DateFormatter.js
@@ -1,4 +1,9 @@
 export default (targetDate) => {
+
+    if (typeof(targetDate) === 'string' || targetDate instanceof String)
+    {
+        targetDate = new Date(targetDate);
+    }
     let formattedDate = targetDate;
     formattedDate = new Date(formattedDate.getTime());
     return formattedDate.toISOString().split('T')[0];
