b2b92076ddf012e58f20d7c53e4eeba00a9b22c4
Roy Gero <roy.gero.ii@gmail.com>
2023-03-07 20:43:06 -0500

    Adding some quick test data for the Lists and helper functions.

---
+++ workout_tracker/src/tests/testData.js
@@ -0,0 +1,100 @@
+export const testWorkouts = [
+    {
+        "workoutDate": "2023-03-08T00:00:00.000Z",
+        "notes": "Testing Notes for Testing",
+        "exerciseList": [
+            {
+                "name": "Bench Press",
+                "muscleGroup": "Chest",
+                "setList": [
+                    {
+                        "reps": 20,
+                        "weight": 275,
+                    },
+                    {
+                        "reps": 20,
+                        "weight": 300,
+                    },
+                    {
+                        "reps": 3,
+                        "weight": 315,
+                    }
+                ],
+                "notes": "Wrist is feeling a bit sore",
+
+            },
+            {
+                "name": "Shrugs",
+                "muscleGroup": "Shoulders",
+                "setList": [
+                    {
+                        "reps": 40,
+                        "weight": 100,
+                    },
+                    {
+                        "reps": 40,
+                        "weight": 100,
+                    },
+                    {
+                        "reps": 40,
+                        "weight": 100,
+                    },
+                    {
+                        "reps": 40,
+                        "weight": 100,
+                    }
+                ],
+                "notes": "Dumbbells",
+            }
+        ],
+    },
+    {
+        "workoutDate": "2023-02-08T00:00:00.000Z",
+        "notes": "February",
+        "exerciseList": [
+            {
+                "name": "Incline Press",
+                "muscleGroup": "Chest",
+                "setList": [
+                    {
+                        "reps": 10,
+                        "weight": 275,
+                    },
+                    {
+                        "reps": 10,
+                        "weight": 300,
+                    },
+                    {
+                        "reps": 3,
+                        "weight": 315,
+                    }
+                ],
+                "notes": "Wrist is feeling a bit sore",
+
+            },
+            {
+                "name": "Lunges",
+                "muscleGroup": "Legs",
+                "setList": [
+                    {
+                        "reps": 40,
+                        "weight": 100,
+                    },
+                    {
+                        "reps": 40,
+                        "weight": 100,
+                    },
+                    {
+                        "reps": 40,
+                        "weight": 100,
+                    },
+                    {
+                        "reps": 40,
+                        "weight": 100,
+                    }
+                ],
+                "notes": "Dumbbells",
+            }
+        ],
+    },
+]
