c0de6c4fd742cd4438a76d654172875ed9f8946d
Roy Gero <roy.gero.ii@gmail.com>
2023-03-12 22:06:03 -0400

    Adding the simple Error Modal to play around with.

---
+++ workout_tracker/src/components/ErrorModal.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+export const ErrorModal = ({closeFunction, errorMessage, shouldBeOpen = false, errorTitle = "Error!"}) => {
+    Modal.setAppElement("div");
+
+    const closeModal = () => {
+        shouldBeOpen = false;
+        closeFunction();
+    } 
+
+    return (
+          <Modal
+            isOpen={shouldBeOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel={errorTitle}
+          >
+            <Container>
+                <h1 className="pb-2">{errorTitle}</h1>
+                <Container className="pb-4">
+                    {errorMessage}
+                </Container>
+                <Button onClick={closeModal}>Close</Button>
+            </Container>
+          </Modal>
+      );
+
+}
--- workout_tracker/src/components/workout/WorkoutForm.js
+++ workout_tracker/src/components/workout/WorkoutForm.js
@@ -9,8 +9,9 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import { ExerciseList } from '../exercise/ExerciseList';
 import { ExerciseForm } from '../exercise/ExerciseForm';
 import DateFormatter from '../../helpers/DateFormatter';
+import {ErrorModal} from '../ErrorModal';
 
-export const WorkoutForm = ({workout = {}, errorMessage, onSubmit})=> 
+export const WorkoutForm = ({workout = {}, onSubmit})=> 
 {
     const [workoutDate, setWorkoutDate] = useState(workout.workoutDate ? DateFormatter(workout.workoutDate) : DateFormatter(new Date()));
     const [exerciseList, setExerciseList] = useState(workout.exerciseList ? workout.exerciseList : []);
@@ -27,6 +28,10 @@ export const WorkoutForm = ({workout = {}, errorMessage, onSubmit})=>
         setExerciseList( [...newExerciseList] );
     }
 
+    const closeModalFunction = () => {
+        setError("");
+    }
+
     const submitWorkout = (e) => {
         e.preventDefault();
         if (!workoutDate)
@@ -35,7 +40,7 @@ export const WorkoutForm = ({workout = {}, errorMessage, onSubmit})=>
         }
         if (exerciseList.length < 1)
         {
-            setError("Need at least one exercise");
+            setError("You need at least one exercise");
         }
         if (error)
         {
@@ -53,55 +58,55 @@ export const WorkoutForm = ({workout = {}, errorMessage, onSubmit})=>
         setExerciseList([]);
         setNotes("");
     }
-
+    
     return (
-        <Container fluid="md">
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item>
-                    <Accordion.Header>
-                        Exercise Information
-                    </Accordion.Header>
-                    <Accordion.Body>
-                        <Form id="addWorkout" onSubmit={submitWorkout}>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Date</InputGroup.Text>
-                                <Form.Control type="date" value={workoutDate} onChange={e => setWorkoutDate(e.target.value)}/>
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Notes</InputGroup.Text>
-                                <Form.Control as="textarea" value={notes} rows={3} onChange={e => setNotes(e.target.value)} />
-                            </InputGroup>
-                        </Form>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item>
-                    <Accordion.Header>
-                        Add/Edit Exercises
-                    </Accordion.Header>
-                    <Accordion.Body>
-                        <ExerciseForm onSubmit={addExercise}/>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
-            { exerciseList.length > 0 ? (
-                <Accordion defaultActiveKey="1">
+            <Container fluid="md">
+                <ErrorModal closeFunction={closeModalFunction} errorMessage={error} shouldBeOpen={error ? true : false}/>
+                <Accordion defaultActiveKey="0">
                     <Accordion.Item>
                         <Accordion.Header>
-                            Exercise List
+                            Exercise Information
                         </Accordion.Header>
                         <Accordion.Body>
-                                <ExerciseList exerciseList={exerciseList} onDelete={deleteExercise}/>
+                            <Form id="addWorkout" onSubmit={submitWorkout}>
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Text>Date</InputGroup.Text>
+                                    <Form.Control type="date" value={workoutDate} onChange={e => setWorkoutDate(e.target.value)}/>
+                                </InputGroup>
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Text>Notes</InputGroup.Text>
+                                    <Form.Control as="textarea" value={notes} rows={3} onChange={e => setNotes(e.target.value)} />
+                                </InputGroup>
+                            </Form>
                         </Accordion.Body>
                     </Accordion.Item>
                 </Accordion>
-            ): (null)
-            }
-            <Button form="addWorkout" variant="outline-secondary" type="submit">
-                    Submit Workout
-            </Button>
-        </Container>
-
-    )
+                <Accordion defaultActiveKey="0">
+                    <Accordion.Item>
+                        <Accordion.Header>
+                            Add/Edit Exercises
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            <ExerciseForm onSubmit={addExercise}/>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+                { exerciseList.length > 0 ? (
+                    <Accordion defaultActiveKey="1">
+                        <Accordion.Item>
+                            <Accordion.Header>
+                                Exercise List
+                            </Accordion.Header>
+                            <Accordion.Body>
+                                    <ExerciseList exerciseList={exerciseList} onDelete={deleteExercise}/>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                ): (null)
+                }
+                <Button form="addWorkout" variant="outline-secondary" type="submit">
+                        Submit Workout
+                </Button>
+            </Container>
+        )
 }
