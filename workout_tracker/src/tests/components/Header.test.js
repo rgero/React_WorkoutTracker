afc64c05304abfacc6f71126495a951cc9709b6a
Roy Gero <roy.gero.ii@gmail.com>
2023-04-02 01:11:40 -0400

    Adding a couple more tests for the header.

--- workout_tracker/src/tests/components/Header.test.js
+++ workout_tracker/src/tests/components/Header.test.js
@@ -17,6 +17,45 @@ test("No user logged in", () => {
     expect(element.toJSON()).toMatchSnapshot()
 });
 
+test("No user logged in - button count", () => {
+    const testValue = {
+        state: {token: null},
+        tryLocalSignin: jest.fn(),
+        signOut: jest.fn()
+    }
+    const element = new TestRenderer.create(
+        <AuthContext.Provider value={testValue}>
+            <Header />
+        </AuthContext.Provider>
+    );
+
+    let buttons = element.root.findAllByType("a");
+    expect(buttons.length).toEqual(3);
+});
+
+test('Branding button works', ()=> {
+    const testValue = {
+        state: {token: null},
+        tryLocalSignin: jest.fn(),
+        signOut: jest.fn()
+    }
+    const element = new TestRenderer.create(
+        <AuthContext.Provider value={testValue}>
+            <Header />
+        </AuthContext.Provider>
+    );
+
+    let buttons = element.root.findAllByType("a");
+    let headerButton = buttons[0].props;
+    let expectedHeader = {
+        href: '/',
+        children: 'Workout Tracker',
+        className: 'navbar-brand'
+    }
+    expect(headerButton).toEqual(expectedHeader);
+})
+
+
 test("User logged in", () => {
     const testValue = {
         state: {token: "aisdnaisndasiudbnuf"},
