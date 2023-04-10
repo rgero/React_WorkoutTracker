5fb3927db652ee7713ff94e5a73e4c4ddae42adb
Roy Gero <roy.gero.ii@gmail.com>
2023-04-10 19:56:46 -0400

    Adding a refresh button. Need to Stylize it.

--- workout_tracker/src/pages/workouts/ViewWorkoutsPage.js
+++ workout_tracker/src/pages/workouts/ViewWorkoutsPage.js
@@ -1,23 +1,40 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import { WorkoutList } from '../../components/workout/WorkoutList';
 import {Context as WorkoutContext} from '../../context/WorkoutContext';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 export const ViewWorkoutsPage = ()=> 
 {
     const {state, fetchWorkouts} = useContext(WorkoutContext);
+    const [loaded, setLoaded] = useState(false);
+
+    const processWorkouts = async () => {
+        setLoaded(false);
+        await fetchWorkouts();
+        setLoaded(true);
+    }
 
     useEffect(()=> {
-        const processWorkouts = async () => {
-            await fetchWorkouts();
-        }
-       processWorkouts();
+        processWorkouts();
     }, []);
-    
+
     return (
         <Container>
-            <h3>Your Workouts</h3>
-            <WorkoutList workoutList={state} />
+            <Row className="space-between">
+                <Col><h3>Your Workouts</h3></Col>
+                <Col><Button onClick={processWorkouts}>Refresh</Button></Col>
+            </Row>
+            
+            { loaded ? (
+                <>
+                    <WorkoutList workoutList={state} />
+                </>
+            ) : (
+                <h3>Loading</h3>
+            )}
         </Container>
     )
 }
