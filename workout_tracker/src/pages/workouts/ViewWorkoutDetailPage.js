9e148dde28784e8abcedbd1c2a93a8c31c824a54
Roy Gero <roy.gero.ii@gmail.com>
2023-04-12 13:20:28 -0400

    Got the Workout View page to go back to the Dashboard and delete a workout if needed. Need to add a pop up comfirmation.

--- workout_tracker/src/pages/workouts/ViewWorkoutDetailPage.js
+++ workout_tracker/src/pages/workouts/ViewWorkoutDetailPage.js
@@ -1,69 +1,119 @@
 import React, { useContext, useEffect, useState } from 'react';
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 import DateFormatter from '../../helpers/DateFormatter';
+
 import {Context as WorkoutContext} from '../../context/WorkoutContext';
 import { ExerciseListItem } from '../../components/exercise/ExerciseListItem';
 
 const ViewWorkoutDetailsPage = ()=> 
 {
-    const {state, fetchWorkoutByID} = useContext(WorkoutContext);
-    const [isLoaded, setLoaded] = useState(false);
+    const navigate = useNavigate();
     const { id } = useParams();
+    
+    const {state, fetchWorkouts, deleteWorkout} = useContext(WorkoutContext);
+    const [currentWorkout, setCurrentWorkout] = useState({});
+    const [isLoaded, setLoaded] = useState(false);
+    const [error, setError] = useState("");
+
+
+    const processDelete = async () => {
+        await deleteWorkout(currentWorkout._id);
+        navigate('/dashboard')
+    }
+
+    const isValidWorkout = () => {
+        return !Object.keys(currentWorkout).length == 0;
+    }
 
     useEffect(()=> {
         const GetWorkoutByID = async (id) => {
-            await fetchWorkoutByID(id);
-            setLoaded(true);
+            if (!isLoaded)
+            {
+                await fetchWorkouts();
+                setLoaded(true);
+            }
+            let loadedWorkout = state.filter( (targetWorkout) => {
+                return id === targetWorkout._id;
+            });
+
+            if (loadedWorkout.length == 0)
+            {
+                setError("An error has occurred");
+            } else {
+                setError("");
+                setCurrentWorkout(loadedWorkout[0]);
+            }
         }
         GetWorkoutByID(id);
-    }, []);
+    }, [fetchWorkouts, id, isLoaded, state]);
 
-    if (state.workoutDate)
-    {
-        return (
-            <Container fluid="md">
-                <h1>{DateFormatter(state.workoutDate)}</h1>
-                <Container>
-                    <Row>
-                        <Col sm={2}>
-                            Notes
-                        </Col>
-                        <Col>
-                            {state.notes}
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col sm={2}>Count</Col>
-                        <Col>{state.exerciseList.length}</Col>
-                    </Row>
-                    <Row>
-                        <Col sm={2}>
-                            Exercises
-                        </Col>
-                        <Col>
-                            <ListGroup>
-                                    {
-                                        state.exerciseList.map((exercise, index) => (
-                                            <ListGroup.Item>
-                                                <ExerciseListItem
-                                                    exercise={exercise}
-                                                />
-                                            </ListGroup.Item>
-                                        ))
-                                    }
-                            </ListGroup>
-                        </Col>
-                    </Row>
+    if (isLoaded) {
+        if (isValidWorkout()) { 
+            return (
+                <Container fluid="md">
+                    <h1>{DateFormatter(currentWorkout.workoutDate)}</h1>
+                    <Container>
+                        <Row>
+                            <Col sm={2}>
+                                Notes
+                            </Col>
+                            <Col>
+                                {currentWorkout.notes}
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm={2}>Count</Col>
+                            <Col>{currentWorkout.exerciseList.length}</Col>
+                        </Row>
+                        <Row>
+                            <Col sm={2}>
+                                Exercises
+                            </Col>
+                            <Col>
+                                <ListGroup>
+                                        {
+                                            currentWorkout.exerciseList.map((exercise, index) => (
+                                                <ListGroup.Item>
+                                                    <ExerciseListItem
+                                                        exercise={exercise}
+                                                    />
+                                                </ListGroup.Item>
+                                            ))
+                                        }
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <Container className="justify-content-center" style={{display:'flex'}}>
+                        <Row>
+                            <Col><Button>Edit</Button></Col>
+                            <Col><Button href="/dashboard">Dashboard</Button></Col>
+                            <Col><Button variant="danger" onClick={processDelete}>Delete</Button></Col>
+                        </Row>
+                    </Container>
                 </Container>
-            </Container>
             )
+        } else if (error)
+        {
+            return(
+                <Container>
+                    {error}
+                </Container>
+            )
+        }
     } else {
-        return (<div>Loading</div>)
+        return (
+            <Container>
+                Loading
+            </Container>
+        )
     }
 }
 
