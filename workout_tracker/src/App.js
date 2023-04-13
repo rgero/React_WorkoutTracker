b4ac98a45a443c3b2b41920d1d325e54eb5b1cc8
Roy Gero <roy.gero.ii@gmail.com>
2023-04-13 11:53:40 -0400

    Getting Rid of the Exercise Context because it's no longer needed due to how I'm changing the data model.

--- workout_tracker/src/App.js
+++ workout_tracker/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider as AuthProvider, Context as AuthContext} from '../src/context/AuthContext'
 import { Provider as WorkoutProvider} from '../src/context/WorkoutContext'
-import { Provider as ExerciseProvider} from '../src/context/ExerciseContext'
 
 import NavigationBar from "./components/Header";
 import IndexPage from './pages/IndexPage';
@@ -16,26 +15,23 @@ import WorkoutDetailPage from './pages/workouts/ViewWorkoutDetailPage';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
   return (
     <AuthProvider>
       <NavigationBar/>
       <WorkoutProvider>
-        <ExerciseProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route index element={<IndexPage/>} />
-              <Route path="login" element={<LoginPage/>} />
-              <Route path="signup" element={<SignUpPage/>} />
-              <Route path="dashboard" element={<DashboardPage/>} />
-              <Route path="about" element={<AboutPage/>} />
-              <Route path="create" element={<AddWorkoutPage/>} />
-              <Route path="edit/:id" element={<EditWorkoutPage/>} />
-              <Route path="view/:id" element={<WorkoutDetailPage/>} />
-            </Routes>
-          </BrowserRouter>
-        </ExerciseProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<IndexPage/>} />
+            <Route path="login" element={<LoginPage/>} />
+            <Route path="signup" element={<SignUpPage/>} />
+            <Route path="dashboard" element={<DashboardPage/>} />
+            <Route path="about" element={<AboutPage/>} />
+            <Route path="create" element={<AddWorkoutPage/>} />
+            <Route path="edit/:id" element={<EditWorkoutPage/>} />
+            <Route path="view/:id" element={<WorkoutDetailPage/>} />
+          </Routes>
+        </BrowserRouter>
       </WorkoutProvider>
     </AuthProvider>
   );
