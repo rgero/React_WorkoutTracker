e80501b0f03d94b44e1455cd308e237dd2e37cf7
Roy Gero <roy.gero.ii@gmail.com>
2023-04-16 21:00:49 -0400

    Setting the descending value to the correct value.

--- workout_tracker/src/pages/workouts/ViewWorkoutsPage.js
+++ workout_tracker/src/pages/workouts/ViewWorkoutsPage.js
@@ -33,7 +33,7 @@ export const ViewWorkoutsPage = ()=>
             <Row>
                 { isLoaded ? (
                     <>
-                        <WorkoutList workoutList={state} descendingOrder={false}/>
+                        <WorkoutList workoutList={state} descendingOrder={true}/>
                     </>
                 ) : (
                     <>Loading</>
