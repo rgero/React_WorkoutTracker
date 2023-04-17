0b1fafdd063ad5ecb1740aabc42a7c8e2728db0b
Roy Gero <roy.gero.ii@gmail.com>
2023-04-16 22:26:25 -0400

    Undoing the changes that are making the snapshot test fail.

--- workout_tracker/src/pages/AboutPage.js
+++ workout_tracker/src/pages/AboutPage.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 function AboutPage() {
     return (
         <Container>
-            <div><a href="https://www.flaticon.com/free-icons/gym" title="gym icons">Gym icons created by Freepik - Flaticon</a></div>
+            <a href="https://www.flaticon.com/free-icons/gym" title="gym icons">Gym icons created by Freepik - Flaticon</a>
         </Container>
     );
   }
