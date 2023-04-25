9206f1516ad80de4a03a28d6e0d9bdf13533a3f7
Roy Gero <roy.gero.ii@gmail.com>
2023-04-25 19:58:59 -0400

    Changing the ExerciseListItem so it doesn't have a button within the collapse.

--- workout_tracker/src/components/exercise/ExerciseListItem.js
+++ workout_tracker/src/components/exercise/ExerciseListItem.js
@@ -11,50 +11,43 @@ import Button from 'react-bootstrap/Button';
 
 export const ExerciseListItem = ({exercise, onDelete})=> 
 {
-    const [optionsShown, showOptions] = useState(false);
-    let exerciseName = exercise.name;
-    let setList = exercise.setList;
+  const [optionsShown, showOptions] = useState(false);
+  let exerciseName = exercise.name;
+  let setList = exercise.setList;
 
-    const processDelete = () => {
-        let title = 'Are you sure you want to delete this?';
-        let subtitle = `The exercise named ${exerciseName}`;
-        let buttons = [
-            {
-              label: 'Yes',
-              onClick: async () => {
-                  await onDelete(exercise);
-              }
-            },
-            {
-              label: 'No'
-            }
-        ]
-        AlertBox(title, subtitle, buttons);
-    }
+  const processDelete = () => {
+    let title = 'Are you sure you want to delete this?';
+    let subtitle = `The exercise named ${exerciseName}`;
+    let buttons = [
+      {
+        label: 'Yes',
+        onClick: async () => {
+          await onDelete(exercise);
+        }
+      },
+      {
+        label: 'No'
+      }
+    ]
+    AlertBox(title, subtitle, buttons);
+  }
 
-    return (
-        <>
-            {
-                exerciseName === "" ? (
-                    <Container className="list-item list-item--message">
-                        <span>No Name</span>
-                    </Container>
-                ) : (
-                    <Container onClick={(e)=> { showOptions(!optionsShown) }}>
-                        {exerciseName}
-                        <SetList setList={setList}/>
-                        <Collapse in={optionsShown}>
-                            <Row>
-                                <Col>
-                                    <Button className="float-end mt-4" variant="danger" onClick={processDelete}>
-                                        Delete
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Collapse>
-                    </Container>
-                )
-            }
-        </>
-    )
+  return (
+    <>
+      {
+        exerciseName === "" ? (
+          <Container className="list-item list-item--message">
+            <span>No Name</span>
+          </Container>
+        ) : (
+            <Container>
+                <Container onClick={processDelete}>
+                    {exerciseName}
+                    <SetList setList={setList} key={`${exerciseName}_Sets`}/>
+                </Container>
+            </Container>
+        )
+      }
+    </>
+  )
 }
