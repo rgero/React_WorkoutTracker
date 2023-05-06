f49b25134d76baa8a1f12c063be83f2b1325cd07
Roy Gero <roy.gero.ii@gmail.com>
2023-05-06 14:15:51 -0400

    Adding some Dashboard tests.

---
+++ workout_tracker/src/tests/pages/DashboardPage.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import reactRouterDom from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
+
+import {Context as AuthContext} from '../../context/AuthContext';
+import DashboardPage from '../../pages/DashboardPage';
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+}));
+
+jest.mock('../../pages/workouts/ViewWorkoutsPage', () => ({
+    ViewWorkoutsPage: ()=> {
+        return (<div>ViewWorkoutsPage Rendered Here</div>);
+    }
+}));
+
+describe("Dashboard Tests", ()=> {
+
+    test('No Token - Redirected to Login', async ()=> {
+        const testContextValues = {
+            state: {},
+            tryLocalSignin: jest.fn()
+        }
+
+        let mockNavigate = jest.spyOn(reactRouterDom, "Navigate").mockImplementation();
+        let element;
+        await act(async () => {
+            element = new TestRenderer.create(
+                <AuthContext.Provider value={testContextValues}>
+                    <DashboardPage/>
+                </AuthContext.Provider>
+            );
+        })
+        
+        expect(mockNavigate.mock.calls).toEqual([[{"replace": true, "to": "/login"}, {}]]);
+    })
+
+    test("Token Presented - matches mocked View Workout Page", async ()=> {
+
+        const testContextValues = {
+            state: { token: "asinaidnsdin"},
+            tryLocalSignin: jest.fn()
+        }
+   
+        let element;
+        await act(async () => {
+            element = new TestRenderer.create(
+                <AuthContext.Provider value={testContextValues}>
+                        <DashboardPage/>
+                </AuthContext.Provider>
+            );
+        })
+
+        expect(element.toJSON()).toMatchSnapshot();
+    })
+})
