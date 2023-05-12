230d824be0f5b4353c768755e7109e93f820a7e1
Roy Gero <roy.gero.ii@gmail.com>
2023-05-11 23:05:41 -0400

    Adding tests for the View Workout and Edit workout buttons

--- workout_tracker/src/tests/components/workouts/WorkoutListItem.test.js
+++ workout_tracker/src/tests/components/workouts/WorkoutListItem.test.js
@@ -1,35 +1,75 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow'
-import {WorkoutListItem} from '../../../components/workout/WorkoutListItem';
 
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import user from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+
+import {Context as WorkoutContext} from '../../../context/WorkoutContext';
+import {WorkoutListItem} from '../../../components/workout/WorkoutListItem';
 import { TestWorkouts } from '../../testData';
 
+const mockedNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
-    useNavigate: jest.fn()
-}));
-
-let realUseContext;
-let useContextMock;
-
-beforeEach(()=> {
-    realUseContext = React.useContext;
-    useContextMock = React.useContext = jest.fn();
-
-    useContextMock.mockReturnValue({
-        deleteWorkout: jest.fn()
-    });
-})
-
-afterEach(()=> {
-    React.useContext = realUseContext;
-})
+    useNavigate: () => mockedNavigate
+}))
 
 describe("Workout List Item - Render tests", ()=> {
+    let realUseContext;
+    let useContextMock;
+
+    beforeEach(()=> {
+        realUseContext = React.useContext;
+        useContextMock = React.useContext = jest.fn();
+    
+        useContextMock.mockReturnValue({
+            deleteWorkout: jest.fn()
+        });
+    })
+    
+    afterEach(()=> {
+        React.useContext = realUseContext;
+    })
+
     test("Has Data, matches snapshot", ()=> {
         const pageRender = new ShallowRenderer().render(
             <WorkoutListItem workout={TestWorkouts[0]} index={1} />
         );
         expect(pageRender).toMatchSnapshot();
+    })
+})
+
+describe("WorkoutListItem - Button Tests", ()=> {
+    beforeEach(()=> {
+        let testContextValues = {
+            deleteWorkout: jest.fn()
+        }
+        render(
+            <WorkoutContext.Provider value={testContextValues}>
+                <WorkoutListItem workout={TestWorkouts[0]} index={1} />
+            </WorkoutContext.Provider>
+        )
+    })
+    
+    test("View Workout", ()=> {
+        let viewButton;
+        act(()=> {
+            viewButton = screen.getByRole("button", {name: /view/i});
+            user.click(viewButton);
+        })
+
+        expect(mockedNavigate).toHaveBeenCalledWith('/view/1234')
+    })
+
+    test("Edit Workout", ()=> {
+        let editButton;
+        act(()=> {
+            editButton = screen.getByRole("button", {name: /edit/i});
+            user.click(editButton);
+        })
+
+        expect(mockedNavigate).toHaveBeenCalledWith('/edit/1234')
     })
 })
